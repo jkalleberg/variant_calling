68cd6b858834f4cbc4bbce4ae4c4914a79a37556
Jenna Kalleberg <66491144+jkalleberg@users.noreply.github.com>
2025-06-13 17:39:00 -0500

    Create start_dev.sh

---
+++ scripts/start_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# scripts/start_dev.sh
+
+# NOTE: To avoid 'conda init' errors:
+# Excute this script with 
+# source scripts/start_dev.sh instead of
+# bash scripts/start_dev.sh
+
+## Activate the new environment ##
+conda activate ./miniconda_envs/dev
