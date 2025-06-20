7e9d76f07cbf53a0edb325b48bee234efb3052c9
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2025-06-20 13:36:10 -0500

    adding generic Dict handler

--- helpers/inputs.py
+++ helpers/inputs.py
@@ -10,6 +10,8 @@ from logging import Logger
 from dataclasses import dataclass
 from pathlib import Path
 from json import load
+from typing import Dict, List, Union
+from sys import exit
 
 @dataclass
 class InputManager:
@@ -65,3 +67,55 @@ class InputManager:
         # if not check_resources:
         #     self.inputs.logger.error(f"{self.inputs.logger_msg}: missing the 'ntasks' SBATCH parameter in resources file | {self.inputs.args.resource_config}\nExiting...")
         #     exit(1)
+    
+    def add_to_dict(
+        self,
+        update_dict: Dict[str, Union[str, int, float]],
+        new_key: str,
+        new_val: Union[str, int, float],
+        valid_keys: Union[List[str], None] = None,
+        replace_value: bool = False,
+    ) -> None:
+        """
+        Add a key=value pair to a dictionary object.
+
+        If the key is missing from the dictionary the 'key=value pair' to the results dictionary.
+
+        Parameters
+        ----------
+        update_dict : Dict[str, Union[str, int, float]]
+            the dictionary object to be altered
+        new_key : str
+            unique hash key
+        new_val : Union[str, int, float]
+            the value returned by the new key
+        valid_keys : Union[List[str], None], optional
+            if provided, compare the new against this list to catch typos or invalid entries, by default None
+        replace_value : bool, optional
+            if True, overwrite the value of an existing key, by default False
+        """
+        if valid_keys is not None:
+            if new_key not in valid_keys:
+                self.logger.error(f"{self.logger_msg}: invalid metadata key | '{new_key}'")
+                valid_key_string: str = ", ".join(valid_keys)
+                self.logger.error(
+                    f"{self.logger_msg}: use one of the following valid keys | '{valid_key_string}'\nExiting..."
+                )
+                exit(1)
+
+        if new_key not in update_dict.keys():
+            update_dict[new_key] = new_val
+            if self.debug_mode:
+                self.logger.debug(f"{self.logger_msg}: dictionary updated with | '{new_key}={new_val}'")
+        elif new_key in update_dict.keys() and replace_value:
+            old_value = update_dict[new_key]
+            update_dict[new_key] = new_val
+            if self.debug_mode:
+                self.logger.debug(
+                    f"{self.logger_msg}: previous value '{new_key}={old_value}' | new value '{new_key}={new_val}'"
+                )
+        else:
+            self.logger.warning(
+                f"{self.logger_msg}: unable to overwrite value for an existing key | '{new_key}'"
+            )
+
