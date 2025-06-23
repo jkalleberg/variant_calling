5146e92f1aeb7d7c6e2b8ab5e3def1c480aee699
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2025-06-23 16:57:33 -0500

    updating to use DeepVariant modules and latest versions

--- scripts/setup/modules.sh
+++ scripts/setup/modules.sh
@@ -21,22 +21,65 @@ conda deactivate
 # Required for pytorch
 module load cuda/11.8.0_gcc_9.5.0
 
+# Singularity command is pre-installed, and does not require a module on Hellbender
+# module load singularity/singularity
+
 # Required for creating the Reference Genome .Dict file which is used to define chromosome names in a species-agnostistic manner
 module load picard/2.26.2_gcc_12.3.0
 
 # Modules required for post-procesing variants
 module load bedtools2/2.31.1
-module load bcftools/1.17
-module load htslib/1.17
-# module load tabix/2013-12-16
+module load bcftools/1.20_gcc_12.3.0
+module load htslib/1.20_gcc_12.3.0
+module load samtools/1.20_gcc_12.3.0
+
+# module load bcftools/1.17
+# module load htslib/1.17
+# module load samtools/1.17
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Done Loading Modules"
 
 echo -e "$(date '+%Y-%m-%d %H:%M:%S') INFO: Conda Base Environment:\n${CONDA_BASE}"
-echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Python Version:"
+
+# Check software versions
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: BCFtools Version -------------------"
+bcftools --version | head -n3
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Python Version -------------------"
 python3 --version
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Java Version -------------------"
+java -version
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Apptainer Version -------------------"
+apptainer --version
+
+# Source DeepVariant version and CACHE Dir
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Adding Apptainer variables... "
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: This step is required to build DeepVariant image(s)"
+
+if [ -z "$1" ]
+then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Using defaults, DeepVariant version 1.4.0"
+    export BIN_VERSION_DV="1.4.0"
+    export BIN_VERSION_DT="1.4.0"
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Using inputs, DeepVariant version $1"
+    export BIN_VERSION_DV="$1"
+    export BIN_VERSION_DT="$1"
+fi
+
+export APPTAINER_CACHEDIR="${PWD}/APPTAINER_CACHE"
+export APPTAINER_TMPDIR="${PWD}/APPTAINER_TMPDIR"
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Done adding Apptainer variables"
+
+# Confirm that it worked
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: DeepVariant Version: ${BIN_VERSION_DV}"
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Apptainer Cache: ${APPTAINER_CACHEDIR}"
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Apptainer Tmp: ${APPTAINER_TMPDIR}"
+
 
 # Activating the Bash Sub-Routine to handle errors
+# NOTE: This helper function are MANDATORY for a module.sh file
+#       because this how I avoid a bash command failing within a SLURM job, 
+#       but the 'SLURM_JOB_STATUS' appears to have worked, when it didn't!
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Loading bash helper functions... "
 source scripts/setup/helper_functions.sh
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Done Loading bash helper functions"
