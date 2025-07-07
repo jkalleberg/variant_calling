326b2db78fce8dc5cb08473526c045b0a51e724c
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2025-07-07 13:19:01 -0500

    switching partition

--- scripts/start_interactive.sh
+++ scripts/start_interactive.sh
@@ -6,9 +6,9 @@
 # altering your account (i.e. 'schnabelr-lab')
 
 # srun --pty -p gpu --time=0-04:00:00 -A schnabelr-lab /bin/bash
-srun --pty -p schnabelr-lab --time=0-05:00:00 --mem=30G -A schnabelr-lab /bin/bash
+# srun --pty -p schnabelr-lab --time=0-05:00:00 --mem=30G -A schnabelr-lab /bin/bash
 # srun --pty -p interactive --time=0-04:00:00 --mem=30G -A schnabelr-lab /bin/bash
 # srun --pty -p interactive --time=0-05:00:00 --mem=30G /bin/bash
-# srun --pty -p general --time=0-08:00:00 --mem=30G /bin/bash
+srun --pty -p general --time=0-08:00:00 --mem=30G /bin/bash
 # srun --pty -p general,schnabelr-umag --time=0-04:00:00 --mem=30G -A schnabelr-umag /bin/bash
 
