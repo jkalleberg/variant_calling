3740eda27517b835ce1ced5051e1084e6c534709
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2025-08-29 17:52:12 -0500

    updating to provide human-trained ckpts

--- scripts/setup/download_models.sh
+++ scripts/setup/download_models.sh
@@ -68,7 +68,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Downloading 7-channel WGS model checkpo
 NO_AF_MODEL_BUCKET="https://storage.googleapis.com/deepvariant/models/DeepVariant/${BIN_VERSION_DV}/DeepVariant-inception_v3-${BIN_VERSION_DV}+data-wgs_standard"
 
 # Use the same file naming convention and enable restarting of any interrupted downloads
-curl --create-dirs --continue-at - "${NO_AF_MODEL_BUCKET}/model.ckpt.{data-00000-of-00001,index,example_info.json,meta}" --output "triotrain/model_training/pretrained_models/v${BIN_VERSION_DV}_withIS_noAF/model.ckpt.#1"
+curl --create-dirs --continue-at - "${NO_AF_MODEL_BUCKET}/model.ckpt.{data-00000-of-00001,index,example_info.json,meta}" --output "tutorial/existing_ckpts/DeepVariant/v${BIN_VERSION_DV}_withIS_noAF/model.ckpt.#1"
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Done - Downloading 7-channel WGS model checkpoint (with InsertSize, but without AlleleFreq)"
 ##========================================================================##
 
@@ -84,7 +84,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Downloading 8-channel WGS model checkpo
 AF_MODEL_BUCKET="https://storage.googleapis.com/brain-genomics-public/research/allele_frequency/pretrained_model_WGS/${BIN_VERSION_DV}"
 
 # Use the same file naming convention and enable restarting of any interrupted downloads
-curl --create-dirs --continue-at - "${AF_MODEL_BUCKET}/model.ckpt.{data-00000-of-00001,index,meta,example_info.json}" --output "triotrain/model_training/pretrained_models/v${BIN_VERSION_DV}_withIS_withAF/wgs_af.model.ckpt.#1"
+curl --create-dirs --continue-at - "${AF_MODEL_BUCKET}/model.ckpt.{data-00000-of-00001,index,meta,example_info.json}" --output "tutorial/existing_ckpts/DeepVariant/v${BIN_VERSION_DV}_withIS_withAF/wgs_af.model.ckpt.#1"
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO: Done - Downloading 8-channel WGS model checkpoint (with InsertSize, with AlleleFreq)"
 
 ##========================================================================##
